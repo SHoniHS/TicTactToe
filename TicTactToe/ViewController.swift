fd15486ff7b73a42f66c65f2a2a4b1fa75aab890
SHS <SHS@macbook-pro-de-shs.local>
2019-01-08 15:51:28 +0100

    Initial Commit

---
+++ TicTactToe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicTactToe
+//
+//  Created by SHS on 8/1/19.
+//  Copyright © 2019 SHS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
